88fdeab4761f40d4eea4a42dfd5284b3c5a92d48
lenakosyakova <kosyakova@gmail.com>
2020-02-06 19:19:04 -0500

    Add require.js, move main app to js/app.js

---
+++ js/app.js
@@ -0,0 +1,206 @@
+
+// models
+
+const Recipe = Backbone.Model.extend({
+  defaults: {
+    "id": null,
+    "image": "",
+    "imageUrls": [
+      ""
+    ],
+    "readyInMinutes": null,
+    "servings": null,
+    "title": ""
+  }
+})
+
+const RecipeDetails = Backbone.Model.extend({
+  defaults: {
+    "id": null,
+    "title": "",
+    "sourceUrl": "",
+    "servings": null,
+    "preparationMinutes": null,
+    "cookingMinutes": null,
+    "readyInMinutes": null,
+    "image": "",
+    "instructions": "",
+  }
+})
+
+// collections
+
+const RecipeList = Backbone.Collection.extend({
+  model: Recipe,
+  localStorage: new Store("backbone-recipes")
+});
+
+// instance of the Collection
+const recipeList = new RecipeList()
+
+// event aggregator
+const eventAggregator = _.extend({}, Backbone.Events)
+
+// views
+
+// renders individual recipe item (li)
+const RecipeView = Backbone.View.extend({
+  tagName: 'li',
+  id: 'recipe-list',
+  template: _.template($('#recipe-template').html()),
+  render: function(){
+    this.$el.html(this.template(this.model.toJSON()))
+    return this // enable chained calls
+  },
+  events: {
+    'click .view': function (e) {
+      console.log('clicked', e)
+      // console.log('clicked id is ', e.target.children[1].innerHTML)
+      eventAggregator.trigger('recipe:selected', this.model)
+    }
+  },
+})
+
+const RecipeDetailsView = Backbone.View.extend({
+  id: 'recipeDetails',
+  model: RecipeDetails,
+  template: _.template($('#recipe-details').html()),
+  render: function(){
+    this.$el.html(this.template(this.model.toJSON()))
+    return this // enable chained calls
+  },
+})
+
+// renders the full list of recipes calling RecipeView for each one
+const AppView = Backbone.View.extend({
+  el: '#app',
+
+  initialize: function(i) {
+    console.log('App View initialize', i)
+    this.input = this.$('#search-recipe')
+    recipeList.on('reset', this.cleanupCollection, this)
+    recipeList.on('add', this.addAllRecipesToCollection, this)
+    this.render()
+  },
+
+  cleanupCollection: function(collection){
+    console.log(`Inside cleanupCollection, got `, collection)
+    // clean the recipe list
+    this.$('#recipe-list').html('')
+  },
+
+  addAllRecipesToCollection: function(recipe){
+    console.log("Got model inside addAllRecipesToCollection", recipe)
+    const view = new RecipeView({model: recipe})
+    $('#recipe-list').append(view.render().el)
+  },
+
+  events: {
+    'keypress #search-recipe': 'searchRecipesOnEnter'
+  },
+
+  searchRecipesOnEnter: function(event) {
+    const query = event.target.value.trim()
+    if (event.keyCode === 13 && query) {
+      // should search recipes using API
+      $.ajax({
+        url: `https://api.spoonacular.com/recipes/search?apiKey=82de3f92015a4978a427335171c863a4&number=6&query=${query}`,
+        success: function (response) {
+          // remove recipe details is any
+          $('#recipeDetails').remove()
+          // clear input
+          $("#search-recipe").val("");
+
+
+          console.log('recipes fetch result: \n', response.results)
+          // reset recipes in collection if any
+          recipeList.reset(Recipe, null)
+
+          // display recipes results in #recipe-list
+          const recipesFetched = _.map(response.results, function(recipe){
+            const recipeItem = new Recipe(recipe)
+            console.log('recipe item: ', recipeItem)
+            return recipeItem
+          })
+          console.log('recipes', recipesFetched)
+          // trigger 'add' event in AppView
+          recipeList.add(recipesFetched)
+          console.log('All new models have been added to the collection')
+          recipeList.trigger('update')
+          // hide recipes collection
+          $("#recipe-list").show()
+        },
+        error: function (xhr) {
+          alert("An error occurred: " + xhr.status + " " + xhr.statusText)
+        }
+      })
+    }
+  },
+})
+
+// Routes
+const Router = Backbone.Router.extend({
+  routes: {
+    'search' : 'showSearchForm',
+    'recipe/:title' : 'showRecipe'
+  },
+
+  showSearchForm: function() {
+    $("#app").show()
+    console.log('Show search form worked')
+  },
+
+  showRecipe: function(title) {
+    //console.log('router.params title = ' + title)
+    // getting list of model instances from collection
+    const listOfRecipes = recipeList.models
+    const selectedRecipe = _.find(listOfRecipes, function (recipe) {
+      //console.log('recipe in find', recipe)
+      //console.log('recipe list in find ', recipeList)
+      return recipe.get('title') === title
+    })
+    //console.log('selected recipe', selectedRecipe)
+    console.log('selected recipe ID ', selectedRecipe.id)
+
+    // fetch from API recipe detailed information to display it on recipe details page
+    const getRecipeInfo = (function() {
+      const id = selectedRecipe.id
+      $.ajax({
+        url: `https://api.spoonacular.com/recipes/${id}/information?apiKey=82de3f92015a4978a427335171c863a4`,
+        success: function (response) {
+          console.log('recipe info fetch response - \n', response)
+          const selectedRecipeInfo = new RecipeDetails(response)
+          console.log('recipe info', selectedRecipeInfo)
+
+          // hide recipes collection
+          $("#recipe-list").hide()
+
+          // remove recipe details if any
+          $('#recipeDetails').remove()
+
+          // show recipe details template
+          $('#main').append(new RecipeDetailsView({model: selectedRecipeInfo}).render().el)
+          //return response
+        },
+        error: function (xhr) {
+          alert("An error occurred: " + xhr.status + " " + xhr.statusText)
+        }
+      })
+    })()
+
+  }
+})
+
+eventAggregator.on('recipe:selected', function (recipe) {
+  const urlPath = 'recipe/' + recipe.get('title')
+  router.navigate(urlPath, {trigger: true})
+})
+
+// initialize router
+const router = new Router()
+Backbone.history.start()
+router.navigate('search', {trigger: true})
+
+// initialize app view
+const appView = new AppView()
+
