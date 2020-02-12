f41fcc0413c9879d8d93cf067cd2349c0d5a8883
lenakosyakova <kosyakova@gmail.com>
2020-02-11 21:03:54 -0500

    Add RecipeCard.jsx react component

---
+++ js/Templates/RecipeCard.jsx
@@ -0,0 +1,21 @@
+define(['react'], function(React) {
+  const RecipeCard = props => {
+    const { id, title, readyInMinutes, image } = props
+    const recipeLink = "#recipe/" + title
+    const imgUrl = "https://spoonacular.com/recipeImages/" + image
+
+    return (
+      <div className="view">
+        <h3 className="recipeTitle" id={id}>
+          <a href={recipeLink}>{title}</a>
+        </h3>
+        <span className="recipeId">{id}</span>
+        <span className="cooktime">Ready in {readyInMinutes} minutes</span>
+        <span className="imgInListWrapper">
+          <img src={imgUrl} alt={title}/>
+        </span>
+      </div>
+    )
+  }
+  return RecipeCard
+})
