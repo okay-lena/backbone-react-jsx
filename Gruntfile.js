bf8554e0fc57392818806ebcdba3e96a6487422e
Lena Kosyakova <kosyakova@gmail.com>
2020-02-14 10:54:05 -0500

    Configure babel for grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,13 +13,31 @@ module.exports = function(grunt) {
       // uglify task configuration goes here
       'js/libs/dest/all.libs.min.js': 'js/libs/dest/all.libs.js',
     },
+    babel: {
+      options: {
+        sourceMap: true
+        // presets specified separately in .babelrc
+      },
+      dist: {
+        files: [    // for multiple files with dynamic names
+          {
+            expand: true,
+            cwd: 'js/Templates/',
+            src: ['*.jsx'],
+            dest: 'js/Templates/',
+            ext: '.js'
+          }
+        ]
+      }
+    },
   })
 
   // Load plugins that provide their tasks
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-babel');
 
   // Default tasks to be run by >>grunt or >>grunt default
   // to avoid running separately >>grunt uglify    etc.
-  grunt.registerTask('default', ['concat', 'uglify']);
+  grunt.registerTask('default', ['concat', 'uglify', 'babel']);
 };
